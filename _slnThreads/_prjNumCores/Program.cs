f160a89dc16c2443ebf252a087baf87ec1641502
Guilherme Dias Gregorio <136377466+Guilherme0364@users.noreply.github.com>
2025-08-28 21:37:07 -0300

    Add files via upload

---
+++ _slnThreads/_prjNumCores/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _prjNumCores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numCores = Environment.ProcessorCount;
+            Console.WriteLine($"A maquina atual possui {numCores} núcleos lógicos");
+
+            for ( int i = 0; i < numCores; i++)
+            {
+                var fluxo = new Thread(Processo);
+                fluxo.Start();
+            }
+        }
+        private static void Processo(object ordem)
+        {
+            Console.WriteLine($"Executado pela Thread {ordem} [{Thread.CurrentThread.ManagedThreadId}]");
+        }
+    }
+}
---
+++ _slnThreads/prjTravamento/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace prjTravamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    internal class TrabalhoTravado()
+    {
+        Thread fluxo1, fluxo2;
+        object RecursoA = new object();
+        object RecursoB = new object();
+        string saida;
+
+        public void iniciaFluxos()
+        {
+            fluxo1 = new Thread(Procedimento1);
+            fluxo2 = new Thread(Procedimento2);
+
+            fluxo1.Start();
+            fluxo2.Start();
+        }
+
+        public string retornaResultado()
+        {
+            fluxo1.Join();
+            fluxo2.Join();
+            return saida; 
+        }
+
+        public void Procedimento1()
+        {
+            Thread.Sleep(100);
+            lock (RecursoA)
+            {
+                saida += "T1#";
+            }
+        }
+
+        public void Procedimento2()
+        {
+            Thread.Sleep(100);
+            lock (RecursoB)
+            {
+                saida += "T1#";
+            }
+        }
+    }
+
+}
